a9e1da6b90ac8a8681171c239d969d9bb27351d6
ferys2195 <fery21irawan.me@gmail.com>
2026-04-02 06:14:09 +0700

    feat : change title

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -6,7 +6,9 @@ export default function IndexPage() {
     <div className="flex h-screen flex-col overflow-hidden p-3">
       <header className="p-3">
         <div className="text-center">
-          <h1 className="mb-1 text-2xl font-bold">TileShift Game</h1>
+          <h1 className="mb-1 font-frijole text-2xl font-bold">
+            Sliding Puzzle Game
+          </h1>
         </div>
       </header>
       <div className="container mx-auto flex-1">
