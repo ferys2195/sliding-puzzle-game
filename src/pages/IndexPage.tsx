a3050b55e75b8fcb9b7054feb7a0e0c738a48277
ferys2195 <fery21irawan.me@gmail.com>
2026-04-03 06:47:05 +0700

    feat : change div to main and apply header component

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,24 +1,16 @@
+import Header from "@/components/Header";
 import Footer from "../components/Footer";
 import Puzzle from "../components/Puzzle";
-import { motion } from "framer-motion";
 
 export default function IndexPage() {
   return (
     <div className="flex size-full h-screen max-h-screen flex-col overflow-hidden bg-gradient-to-br from-slate-900 via-purple-900 to-slate-900 p-3">
       <header className="p-3">
-        <motion.div
-          initial={{ y: -50, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          className="mb-8 text-center"
-        >
-          <h1 className="mb-2 bg-gradient-to-r from-yellow-400 via-orange-500 to-red-500 bg-clip-text font-frijole text-4xl font-bold text-transparent sm:text-5xl">
-            SLIDING PUZZLE
-          </h1>
-        </motion.div>
+        <Header />
       </header>
-      <div className="container mx-auto max-w-7xl flex-1">
+      <main className="container mx-auto max-w-7xl flex-1">
         <Puzzle />
-      </div>
+      </main>
       <footer>
         <Footer />
       </footer>
