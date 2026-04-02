7554cf52fbdb8bef8bcf0a1966b91c111912b0b1
ferys2195 <fery21irawan.me@gmail.com>
2026-04-03 06:34:59 +0700

    change structure of page

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,20 +1,25 @@
 import Footer from "../components/Footer";
 import Puzzle from "../components/Puzzle";
+import { motion } from "framer-motion";
 
 export default function IndexPage() {
   return (
-    <div className="flex h-screen flex-col overflow-hidden p-3">
+    <div className="flex size-full h-screen max-h-screen flex-col overflow-hidden bg-gradient-to-br from-slate-900 via-purple-900 to-slate-900 p-3">
       <header className="p-3">
-        <div className="text-center">
-          <h1 className="mb-1 font-frijole text-2xl font-bold">
-            Sliding Puzzle Game
+        <motion.div
+          initial={{ y: -50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          className="mb-8 text-center"
+        >
+          <h1 className="mb-2 bg-gradient-to-r from-yellow-400 via-orange-500 to-red-500 bg-clip-text font-frijole text-4xl font-bold text-transparent sm:text-5xl">
+            SLIDING PUZZLE
           </h1>
-        </div>
+        </motion.div>
       </header>
-      <div className="container mx-auto flex-1">
+      <div className="container mx-auto max-w-7xl flex-1">
         <Puzzle />
       </div>
-      <footer className="container mx-auto">
+      <footer>
         <Footer />
       </footer>
     </div>
